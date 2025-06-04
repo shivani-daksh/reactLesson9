0d3589d7a1ed1a716fde6a53a78890c7d75a1de3
shivani daksh <shivanidaksh2311@gmail.com>
2025-06-04 17:02:03 +0530

    (9b) unit test to check if formatMoney works with -ve numbers

--- ecommerce-project/src/utils/money.js
+++ ecommerce-project/src/utils/money.js
@@ -1,3 +1,10 @@
 export function formatMoney(amountCents) {
+   if(amountCents < 0){
+      // - Switch the negative number to positive using * -1.
+    // - Put the negative sign at the front of the result.
+    amountCents = amountCents * -1;
+    return `-$${((amountCents) / 100).toFixed(2)}`;
+   }
+
    return `$${(amountCents / 100).toFixed(2)}` ;
 }
--- ecommerce-project/src/utils/money.test.js
+++ ecommerce-project/src/utils/money.test.js
@@ -16,5 +16,10 @@ it('displays 2 decimals', ()=> {
 it('works with the number 0', ()=>{
 expect(formatMoney(0)).toBe('$0.00');
 });
+
+it('works with negetive numbers', ()=>{
+  expect(formatMoney(-999)).toBe('-$9.99');
+  expect(formatMoney(-100)).toBe('-$1.00');
+})
 });
 
